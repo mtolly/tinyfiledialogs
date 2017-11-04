9f16ff97d1b09d2c431ae870f4a8c70f8c07caaa
Michael Tolly <miketolly@gmail.com>
2017-11-04 00:57:39 -0500

    Fix windows ghci undefined symbols

--- cbits/tinyfiledialogs.c
+++ cbits/tinyfiledialogs.c
@@ -88,7 +88,8 @@ misrepresented as being the original software.
 3. This notice may not be removed or altered from any source distribution.
 */
 
-#define _POSIX_C_SOURCE 2 /* to accept POSIX 2 in old ANSI C standards */
+// MT: removing because this breaks Windows GHCi. don't know why!
+// #define _POSIX_C_SOURCE 2 /* to accept POSIX 2 in old ANSI C standards */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
